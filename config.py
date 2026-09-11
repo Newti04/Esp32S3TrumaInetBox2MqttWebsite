87a1b8a681b814141dfea0b6cb178457f62ef6d6
Newti04 <126014337+Newti04@users.noreply.github.com>
2026-09-11 19:21:32 +0200

    Create config.py with initial configuration settings
    Added configuration settings for WiFi, MQTT, and hardware.

---
+++ config.py
@@ -0,0 +1,22 @@
+{
+    "wifi": {
+        "ssid": "DEIN_WLAN_NAME",
+        "password": "DEIN_WLAN_PASSWORT",
+        "ap_ssid": "Truma_iNet_Fallback",
+        "ap_password": "InsecurePassword123",
+        "conn_timeout": 15
+    },
+    "mqtt": {
+        "broker": "192.168.178.50",
+        "port": 1883,
+        "user": "",
+        "password": "",
+        "client_id": "inetbox2mqtt_esp32s3",
+        "topic_prefix": "truma"
+    },
+    "hardware": {
+        "uart_id": 1,
+        "tx_pin": 17,
+        "rx_pin": 18
+    }
+}
